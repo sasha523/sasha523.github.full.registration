ad2f998429688bb1aa67744165f9766ceb02ae1d
Alexander <75177317+sasha523@users.noreply.github.com>
2021-07-26 21:02:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- Форма авторизации -->
+
+    <form action="vendor/signin.php" method="post">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <button type="submit">Войти</button>
+        <p>
+            У вас нет аккаунта? - <a href="/register.php">зарегистрируйтесь</a>!
+        </p>
+        <?php
+            if ($_SESSION['message']) {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+        ?>
+    </form>
+
+</body>
+</html>
+       
